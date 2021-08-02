16bbf72de5ba45733efa5ff259220413b6b1d744
larrydor <86853201+larrydor@users.noreply.github.com>
2021-08-02 12:38:43 -0400

    Update movie.py

--- movie.py
+++ movie.py
@@ -8,8 +8,8 @@ load_dotenv()
 API = os.getenv("RAPID_API")
 
 print("-------------------------")
-x = input("Select your sports team: ")
-print("Sports team selected: ", x)
+x = input("Film Title you seek to research: ")
+print("Title selected: ", x)
 print("Let me look that up for you...")
 
 conn = http.client.HTTPSConnection("imdb8.p.rapidapi.com")
@@ -24,7 +24,7 @@ conn.request("GET", "/auto-complete?q=", headers=headers)
 res = conn.getresponse()
 data = res.read()
 
-print(data.decode("utf-8"))
+#print(data.decode("utf-8"))
 
 url = "https://imdb8.p.rapidapi.com/auto-complete"
 
@@ -38,9 +38,30 @@ headers = {
 
 response = requests.request("GET", url, headers=headers, params=querystring)
 parsed_response = json.loads(response.text)
-print(type(parsed_response))
-print(parsed_response.keys())
+#print(type(parsed_response))
+#print(parsed_response.keys())
 #print(parsed_response)
-pprint(response.text)
+#pprint(response.text)
 
+film_name = [movies["l"] for movies in parsed_response["d"]]
+#print(film_name)
+film_year = [movies["y"] for movies in parsed_response["d"]]
+#print(film_year)
+#print(film_name, film_year)
 
+# Function to convert
+# source: https://www.geeksforgeeks.org/python-program-to-convert-a-list-to-string/
+def listToString(s): 
+    
+    # initialize an empty string
+    str1 = "" 
+    
+    # traverse in the string  
+    for ele in s: 
+        str1 += ele  
+    
+    # return string  
+    return str1 
+
+print(listToString(film_name))
+print(film_name, film_year)
